0861009b5554163364de636f42eec6fb1fc4a685
DanielC19 <danysam1010@gmail.com>
2024-02-08 11:29:50 -0500

    busqueda de peliculas

--- movie_reviews/settings.py
+++ movie_reviews/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for movie_reviews project.
 
@@ -14,6 +16,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
